07adc0e6f67ab43a28a54670dac2bee5b5112e8d
Mike <mike_christensen@btinternet.com>
2015-12-27 17:54:47 +0000

    now reads a query folder of consecutively numbered images. Prints out time to match for each image.

--- query.cpp
+++ query.cpp
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <iomanip>
+#include <sys/time.h>
 #include "surf.hpp"
 #include "saveable_matcher.hpp"
 
 using namespace cv;
-
-#define NUM_TRAINING_IMAGES 30
 
 void DIE(const char* message)
 {
@@ -30,36 +30,69 @@ void DIE(const char* message)
 *     - Reject matches below a certain euclidean distance, and still threshold on number of matches?
 */
 
+void readQueryImages(std::string const &folderpath, std::string const &extension, int number, std::vector<Mat> &images)
+{
+  for(int i = 1; i <= number; i++)
+  {
+    printf("Reading image %d\n", i);
+    std::stringstream ss;
+    ss << folderpath << std::setfill('0') << std::setw(4) << std::to_string(i) << extension;
+    std::string s = ss.str();
+    Mat im = imread(s, 1);
+    images.push_back(im);
+    if(!im.data) {
+      DIE("Missing training image data!");
+    }
+  }
+}
+
 int main( int argc, char** argv )
 {
-  char* queryImageName = argv[1];
-  Mat queryImage;
-  queryImage = imread( queryImageName, 1 );
-  if(!queryImage.data)
-  {
-    DIE("Missing image data!");
-  }
+  char* queryFolderName = argv[1];
+  int number = atoi(argv[2]);
+  strcat(queryFolderName, "/");
+  std::string const extension = ".jpg";
 
+  std::vector<Mat> queryImages;
+  readQueryImages(queryFolderName, extension, number, queryImages);
+
+  //TODO: port this to library call so same parameters used here and in training
   Ptr<xfeatures2d::SURF> detector = xfeatures2d::SURF::create(400.0, 4, 2, 1, 0);
 
-  //detect keypoints and compute descriptors of query image using the detector
-  std::vector<KeyPoint> keypoints;
-  Mat descriptors;
-  detector->detectAndCompute(queryImage, noArray(), keypoints, descriptors, false);
-
-  //Create a matcher based on the model data
+  // Load a matcher based on the model data
   Ptr<SaveableFlannBasedMatcher> matcher = new SaveableFlannBasedMatcher("wills");
   printf("Loading matcher..\n");
   matcher->load();
   printf("Loaded!\n");
 
-  //KNN match the query image to the training set with N=2
+
   std::vector<std::vector<DMatch> > knn_matches;
   std::vector<DMatch> matches;
-  matcher->knnMatch(descriptors, knn_matches, 2);
+  std::vector<KeyPoint> keypoints;
+  struct timeval timstr;
+  double tic,toc;
+  for(int i = 0; i < queryImages.size(); i++)
+  {
+    knn_matches.clear();
+    matches.clear();
+    keypoints.clear();
 
-  //Filter the matches according to a threshold
-  loweFilter(knn_matches, matches);
+    gettimeofday(&timstr,NULL);
+    tic=timstr.tv_sec+(timstr.tv_usec/1000000.0);
 
-  printf("Query has %lu matches with training set\n", matches.size());
+    //detect keypoints and compute descriptors of query image using the detector
+    Mat descriptors;
+    detector->detectAndCompute(queryImages.at(i), noArray(), keypoints, descriptors, false);
+
+    //KNN match the query images to the training set with N=2
+    matcher->knnMatch(descriptors, knn_matches, 2);
+
+    //Filter the matches according to a threshold
+    loweFilter(knn_matches, matches);
+
+    gettimeofday(&timstr,NULL);
+    toc=timstr.tv_sec+(timstr.tv_usec/1000000.0);
+
+    printf("Query %d:\t%lu matches.\t\t(Elapsed Time: %.6lf)\n", i, matches.size(), toc-tic);
+  }
 }
