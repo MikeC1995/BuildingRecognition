08d9c021282faa0bb215a09cf897458240c251b0
Mike Christensen <mike_christensen@btinternet.com>
2016-01-18 07:29:10 -0500

    loads matcher, can use opencv to read images. However, OutOfMemory error when calling OpenCV detectAndCompute - need a custom wrapper for the cpp implementations

--- src/server.py
+++ src/server.py
@@ -1,4 +1,20 @@
+import cv2
 import saveable_matcher
+
 matcher = saveable_matcher.SaveableFlannBasedMatcher("wills")
 matcher.test()
 matcher.load()
+print "Loaded matcher!"
+
+image = cv2.imread("/root/server/images/wills/query/0001.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+sift = cv2.xfeatures2d.SIFT_create()
+
+# Methods below cause OutOfMemoryError. Instead, wrap surf.cpp modules
+# for python. Need to be able to:
+#   -Detect and compute kps and descs for query image
+#   -call matcher->knnMatch on loaded matcher
+#   -call loweFilter in surf module
+
+(kps, descs) = sift.detectAndCompute(gray, None)
+print("# kps: {}, descriptors: {}".format(len(kps), descs.shape))
