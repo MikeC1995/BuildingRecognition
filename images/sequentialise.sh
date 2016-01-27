5676ab01c04f6ed08f6693d2c82b93bb8efb70c0
Mike Christensen <mike_christensen@btinternet.com>
2016-01-27 07:43:59 -0500

    now also processes .jpeg files

--- images/sequentialise.sh
+++ images/sequentialise.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Looking for images in:" $1 "..."
 a=1
-for i in $1/*.jpg $1/*.JPG; do
+for i in $1/*.jpg $1/*.JPG $1/*.jpeg; do
   new=$(printf $1/"%04d.jpg" "$a") #04 pad to length of 4
   mv -- "$i" "$new"
   let a=a+1
