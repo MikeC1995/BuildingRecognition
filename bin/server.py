a8e58ae49a3ed40d9fa77668df765f5ce84acc55
Mike Christensen <mike_christensen@btinternet.com>
2016-02-26 12:38:25 -0500

    POST on /sv : fetches SV images and saves to disk

--- bin/server.py
+++ bin/server.py
@@ -6,6 +6,11 @@ from flask import Flask
 from flask import request, redirect, url_for, send_from_directory, jsonify
 from werkzeug import secure_filename
 
+import requests # for performing our own HTTP requests
+import math
+from PIL import Image
+
+
 ###### ACCESS CONTROL ##########
 
 from datetime import timedelta
@@ -108,6 +113,67 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
+# Saves each Street View image for a lat-lng with heading increments of theta
+# Tests for no available image by pixel color
+# Files written as sv/<lat>,<lng>-<heading>.jpg
+def saveSVImages(lat,lng,theta):
+    # Street View api key
+    key = 'AIzaSyCP5BKla9RY0aObtlovjVzIBV2XEsfYj48'
+
+    # For each heading
+    for i in range(0, int(math.floor(360/theta)),1):
+        heading = i * theta
+
+        # Build the url to get the SV image at this location and heading
+        url = 'https://maps.googleapis.com/maps/api/streetview?size=640x640'
+        url += ('&location={},{}'.format(lat,lng))
+        url += ('&fov={}'.format(theta))
+        url += ('&heading={}'.format(heading))
+        url += '&pitch=35'
+        url += ('&key={}'.format(key))
+
+        # Get the image, stream it to file
+        r = requests.get(url,stream=True)
+        if r.status_code == 200:
+            # open new file for writing and reading, binary
+            filename = 'sv/{},{}-{}.jpg'.format(lat,lng,heading)
+            with open(filename, 'wb+') as f:
+                for chunk in r:
+                    f.write(chunk)
+            # Open the newly written image to read pixel data
+            im = Image.open(filename)
+            # if first pixel is "no image" grey then no SV data at this location
+            # so delete image and return
+            if im.load()[0,0] == (228,227,223):
+                print "No imagery at {},{}".format(lat,lng)
+                os.remove(filename)
+                return
+        else:
+            print "Error getting sv image!"
+
+@app.route('/sv', methods=['POST'])
+def sv():
+    # read args from POST form data
+    args = request.form
+    lat1 = float(args.get('lat1'))
+    lng1 = float(args.get('lng1'))
+    lat2 = float(args.get('lat2'))
+    lng2 = float(args.get('lng2'))
+    density = int(args.get('density'))
+    theta = int(args.get('theta'))
+
+    # iterate over mesh of lat-lngs at specified density,
+    # producing SV images at each point
+    lat_step = (lat1 - lat2)/density
+    lng_step = (lng1 - lng2)/density
+    for i in range(0,density+1,1):
+        for j in range(0,density+1,1):
+            lat = lat2 + j * lat_step
+            lng = lng2 + i * lng_step
+            saveSVImages(lat,lng,theta)
+
+    return jsonify(success='true')
+
 if __name__ == '__main__':
-    #app.debug = True
+    app.debug = True
     app.run(host='0.0.0.0')
